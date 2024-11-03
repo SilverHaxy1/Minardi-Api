7a677ff8a2dcbd30e31e7b2b9db5561efa3cab4d
Arthur <arthurmodena1@gmail.com>
2024-11-03 19:36:08 -0300

    refactor: config route create user and controller

--- src/constrollers/user.controller.js
+++ src/constrollers/user.controller.js
@@ -1,7 +1,20 @@
-const soma = (req, res) => {
-    const soma = 100 + 1;
+const create = (req, res) => {
+    const { name, username, email, password } = req.body;
 
-    res.send({soma: soma});
+    if (!name || !username || !email || !password) {
+        return res.status(400).send({ message: "Submit all fields for registration!" });
+    }
+
+
+    res.status(201).send({
+        message: "User created Sucessfully",
+        user: {
+            name, 
+            username,
+            email,
+            password
+        }
+    })
 };
 
-module.exports = { soma };
+module.exports = { create };
